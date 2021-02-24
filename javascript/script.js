f097db15ff748143cdf7b37cd06639f5b4901fc8
Stacey Muscat <staceymuscat@gmail.com>
2021-02-25 00:08:07 +1100

    Create operate function

--- javascript/script.js
+++ javascript/script.js
@@ -19,3 +19,18 @@ console.log(add(1,2));
 console.log(subtract(1,2));
 console.log(multiply(1,2));
 console.log(divide(1,2));
+
+function operate (x, operator, y) {
+  if (operator == add) {
+    return add(x, y);
+  } else if (operator == subtract) {
+    return subtract(x, y);
+  } else if (operator == multiply) {
+    return multiply(x, y);
+  } else if (operator == divide){
+    return divide(x, y);
+  } return;
+}
+
+// Console Log
+console.log(operate(10, divide, 2));
