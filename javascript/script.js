0f21901cc93b572abfb4fedf441051212b535cb8
Stacey Muscat <staceymuscat@gmail.com>
2021-02-25 00:00:18 +1100

    Add console.log for operators

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+function add (x, y) {
+	return x + y;
+}
+
+function subtract (x, y) {
+	return x - y;
+}
+
+function multiply (x, y) {
+	return x * y;
+}
+
+function divide (x, y) {
+  return x / y;
+}
+
+// Console Log
+console.log(add(1,2));
+console.log(subtract(1,2));
+console.log(multiply(1,2));
+console.log(divide(1,2));
